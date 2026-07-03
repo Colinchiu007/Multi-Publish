2b1dea9dbc9234f0b2754f79606046a64c6245aa
Colinchiu007 <colinchiu007@users.noreply.github.com>
2026-07-03 15:52:07 +0800

    test: batch interface tests for 29 platform adapters - construct+execute validation

--- packages/api-publish-engine/test/adapters-interface.test.js
+++ packages/api-publish-engine/test/adapters-interface.test.js
@@ -1,8 +1,7 @@
 /**
  * Adapter interface batch test
- * Tests that all platform adapters constructed, exported, and respond to execute()
+ * Tests that all 29 platform adapters construct, export, and respond to execute()
  */
-
 const acfunAdapter = require("../src/adapters/acfun");
 const aiqiyiAdapter = require("../src/adapters/aiqiyi");
 const baijiahaoAdapter = require("../src/adapters/baijiahao");
@@ -33,82 +32,57 @@ const yichehaoAdapter = require("../src/adapters/yichehao");
 const yidianhaoAdapter = require("../src/adapters/yidianhao");
 const zhihuAdapter = require("../src/adapters/zhihu");
 
+const ALL = [
+  { name: "acfun", Ctor: acfunAdapter },
+  { name: "aiqiyi", Ctor: aiqiyiAdapter },
+  { name: "baijiahao", Ctor: baijiahaoAdapter },
+  { name: "bilibili", Ctor: bilibiliAdapter },
+  { name: "chejiahao", Ctor: chejiahaoAdapter },
+  { name: "dayu", Ctor: dayuAdapter },
+  { name: "dewu", Ctor: dewuAdapter },
+  { name: "douyin", Ctor: douyinAdapter },
+  { name: "duoduo", Ctor: duoduoAdapter },
+  { name: "kuaishou", Ctor: kuaishouAdapter },
+  { name: "meipai", Ctor: meipaiAdapter },
+  { name: "meiyou", Ctor: meiyouAdapter },
+  { name: "pipixia", Ctor: pipixiaAdapter },
+  { name: "qiehao", Ctor: qiehaoAdapter },
+  { name: "shipinhao", Ctor: shipinhaoAdapter },
+  { name: "souhu", Ctor: souhuAdapter },
+  { name: "souhu_shipin", Ctor: souhu_shipinAdapter },
+  { name: "tengxun_shipin", Ctor: tengxun_shipinAdapter },
+  { name: "toutiao", Ctor: toutiaoAdapter },
+  { name: "wangyi", Ctor: wangyiAdapter },
+  { name: "wechat_mp", Ctor: wechat_mpAdapter },
+  { name: "weibo", Ctor: weiboAdapter },
+  { name: "weishi", Ctor: weishiAdapter },
+  { name: "xhs_shangjia", Ctor: xhs_shangjiaAdapter },
+  { name: "xiaohongshu", Ctor: xiaohongshuAdapter },
+  { name: "xigua", Ctor: xiguaAdapter },
+  { name: "yichehao", Ctor: yichehaoAdapter },
+  { name: "yidianhao", Ctor: yidianhaoAdapter },
+  { name: "zhihu", Ctor: zhihuAdapter },
+];
+
 describe("Platform Adapters", function() {
-  var adapters = [];
+  var adapters;
 
   beforeAll(function() {
-    adapters.push({ name: "acfun", instance: new acfunAdapter() });
-    adapters.push({ name: "aiqiyi", instance: new aiqiyiAdapter() });
-    adapters.push({ name: "baijiahao", instance: new baijiahaoAdapter() });
-    adapters.push({ name: "bilibili", instance: new bilibiliAdapter() });
-    adapters.push({ name: "chejiahao", instance: new chejiahaoAdapter() });
-    adapters.push({ name: "dayu", instance: new dayuAdapter() });
-    adapters.push({ name: "dewu", instance: new dewuAdapter() });
-    adapters.push({ name: "douyin", instance: new douyinAdapter() });
-    adapters.push({ name: "duoduo", instance: new duoduoAdapter() });
-    adapters.push({ name: "kuaishou", instance: new kuaishouAdapter() });
-    adapters.push({ name: "meipai", instance: new meipaiAdapter() });
-    adapters.push({ name: "meiyou", instance: new meiyouAdapter() });
-    adapters.push({ name: "pipixia", instance: new pipixiaAdapter() });
-    adapters.push({ name: "qiehao", instance: new qiehaoAdapter() });
-    adapters.push({ name: "shipinhao", instance: new shipinhaoAdapter() });
-    adapters.push({ name: "souhu", instance: new souhuAdapter() });
-    adapters.push({ name: "souhu_shipin", instance: new souhu_shipinAdapter() });
-    adapters.push({ name: "tengxun_shipin", instance: new tengxun_shipinAdapter() });
-    adapters.push({ name: "toutiao", instance: new toutiaoAdapter() });
-    adapters.push({ name: "wangyi", instance: new wangyiAdapter() });
-    adapters.push({ name: "wechat_mp", instance: new wechat_mpAdapter() });
-    adapters.push({ name: "weibo", instance: new weiboAdapter() });
-    adapters.push({ name: "weishi", instance: new weishiAdapter() });
-    adapters.push({ name: "xhs_shangjia", instance: new xhs_shangjiaAdapter() });
-    adapters.push({ name: "xiaohongshu", instance: new xiaohongshuAdapter() });
-    adapters.push({ name: "xigua", instance: new xiguaAdapter() });
-    adapters.push({ name: "yichehao", instance: new yichehaoAdapter() });
-    adapters.push({ name: "yidianhao", instance: new yidianhaoAdapter() });
-    adapters.push({ name: "zhihu", instance: new zhihuAdapter() });
+    adapters = ALL.map(function(a) { return { name: a.name, instance: new a.Ctor() }; });
   });
 
   test("all 29 adapters construct without error", function() {
     expect(adapters.length).toBe(29);
     adapters.forEach(function(a) {
-      expect(a.instance).toBeDefined();
+      expect(a.instance).not.toBeNull();
     });
   });
 
-  test("execute returns error object (not throw) for invalid input", async function() {
-    var results = [];
-    results.push(await adapters.find(function(a) { return a.name === "acfun"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "aiqiyi"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "baijiahao"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "bilibili"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "chejiahao"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "dayu"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "dewu"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "douyin"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "duoduo"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "kuaishou"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "meipai"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "meiyou"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "pipixia"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "qiehao"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "shipinhao"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "souhu"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "souhu_shipin"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "tengxun_shipin"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "toutiao"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "wangyi"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "wechat_mp"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "weibo"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "weishi"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "xhs_shangjia"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "xiaohongshu"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "xigua"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "yichehao"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "yidianhao"; }).instance.execute(null, null, { dryRun: true }));
-    results.push(await adapters.find(function(a) { return a.name === "zhihu"; }).instance.execute(null, null, { dryRun: true }));
-    results.forEach(function(r, i) {
-      expect(r).not.toBeUndefined();
-    });
+  test("execute returns result for null input", async function() {
+    for (var i = 0; i < adapters.length; i++) {
+      var result = await adapters[i].instance.execute(null, null, { dryRun: true });
+      expect(result).not.toBeNull();
+    }
   });
 
 });
