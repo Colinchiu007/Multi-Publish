84296622be809f7a3d5b1dc3f3a0811bbf65d482
Colinchiu007 <colinchiu007@users.noreply.github.com>
2026-07-03 10:19:12 +0800

    chore(upload): 更新upload/index.js导出token-acquirer

--- packages/api-publish-engine/upload/index.js
+++ packages/api-publish-engine/upload/index.js
@@ -1,1 +1,10 @@
-module.exports = { getUploadProvider: require("./orchestrator").getUploadProvider, upload: require("./orchestrator").upload, CosProvider: require("./providers/cos-provider"), OssProvider: require("./providers/oss-provider"), HttpProvider: require("./providers/http-provider") };
+module.exports = {
+  getUploadProvider: require("./orchestrator").getUploadProvider,
+  upload: require("./orchestrator").upload,
+  getAcquirer: require("./token-acquirer").getAcquirer,
+  getCachedToken: require("./token-acquirer").getCachedToken,
+  clearTokenCache: require("./token-acquirer").clearCache,
+  CosProvider: require("./providers/cos-provider"),
+  OssProvider: require("./providers/oss-provider"),
+  HttpProvider: require("./providers/http-provider")
+};
