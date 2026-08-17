29a25c059a5e91200f93c365e84fc76b9f9c204f
Colin Chiu <colin@qclaw.qq.com>
2026-08-17 23:29:18 +0800

    feat(ops-center): explain operations center pages

--- ops-center/frontend/src/App.vue
+++ ops-center/frontend/src/App.vue
@@ -131,14 +131,18 @@
       </div>
     </el-aside>
     <el-main>
+      <PageGuide v-if="route.meta.requiresAuth" :guide="pageGuide" />
       <router-view />
     </el-main>
   </el-container>
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import { useAuthStore } from './stores/auth'
+import PageGuide from './components/PageGuide.vue'
+import { getPageGuide } from './pageGuides'
 
 const route = useRoute()
 const authStore = useAuthStore()
@@ -146,6 +150,8 @@ const authStore = useAuthStore()
 function logout() {
   authStore.logout()
 }
+
+const pageGuide = computed(() => getPageGuide(route.name))
 </script>
 
 <style>
